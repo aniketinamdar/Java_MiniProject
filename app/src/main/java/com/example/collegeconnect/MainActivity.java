64c8151cf8b811ff97cf1799b80a839b9d47c318
Aniket <aniketinamdar02@gmail.com>
2022-05-30 00:37:44 +0530

    Edit profile done

--- app/src/main/java/com/example/collegeconnect/MainActivity.java
+++ app/src/main/java/com/example/collegeconnect/MainActivity.java
@@ -30,6 +30,6 @@ public class MainActivity extends AppCompatActivity {
     public void Finish(View view) {
         finish();
     }
-
+    public void Edit(View view){startActivity(new Intent(this,EditProfile.class));}
 
 }
